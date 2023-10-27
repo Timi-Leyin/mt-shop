15e0c2a8ebac1a74f607095409ad829073f91df6
OriginalTimi <timileyinoyelekan11@gmail.com>
2023-10-27 16:37:15 +0100

    feat: setup font

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import './styles/fonts.css'
 import './styles/index.css'
 import { BrowserRouter } from 'react-router-dom'
 import Lenis from '@studio-freight/lenis'
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   return (
-    <div className="text-5xl h-screen py-12">lorwm</div>
+    <div className="text-5xl h-screen py-12">Hello</div>
   )
 }
 
