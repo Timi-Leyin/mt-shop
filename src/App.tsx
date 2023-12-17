04340eff8b4eeaedbe89a9745986365356ba6b09
Original Timi <timileyinoyelekan11@gmail.com>
2023-12-17 18:59:08 +0100

    feat: configure routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import {Route, Routes} from "react-router-dom";
 import Home from "./pages";
+import Contact from "./pages/Contact";
+import Shop from "./pages/Shop";
+import About from "./pages/About";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/shop" element={<Shop />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
   )
 }
