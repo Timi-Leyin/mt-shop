9b0cb79c4e5a5aec823e91ffe114d602af89136a
Original Timi <timileyinoyelekan11@gmail.com>
2023-12-17 19:00:18 +0100

    style: added banner to display ads

---
+++ src/components/EventsBanner.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const EventsBanner = () => {
+  return (
+    <div className='w-full h-[35vh] bg-[#eee] p-10'>- Advertisments</div>
+  )
+}
+
+export default EventsBanner
