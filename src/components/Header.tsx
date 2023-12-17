acde1726af604777393eb8479f88ad6d24e96341
Original Timi <timileyinoyelekan11@gmail.com>
2023-12-17 19:00:34 +0100

    fix: cart popup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,13 +9,15 @@ import {
 } from "iconsax-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import Cart from "./Cart";
 
 const Header = () => {
   const [variant, _] = useState<IconProps["variant"]>("Bold");
   const SIZE = 18;
+  const [openCart, setOpenCart] = useState(false)
 
   return (
-    <header className="flex absolute z-[9] w-full text-white top-0 left-0 px-10 justify-between">
+    <><header className="flex absolute z-[9] w-full py-2 text-white top-0 left-0 px-10 justify-between">
       <ul className="hidden gap-4 w-3/4 md:flex  border-b-[1px] border-gray-200 items-center">
         <li className="">
           <Link to="/">Home</Link>
@@ -35,11 +37,11 @@ const Header = () => {
       </ul>
 
       <div className="w-3/4  border-b-[1px] border-gray-200  md:hidden hamburger">
-       <HambergerMenu />
+        <HambergerMenu />
       </div>
 
       <div className="logo m-3 mx-10">
-        <img src="/assets/logo.png" className="" width={100} alt="" />
+        {/* <img src="/assets/logo.png" className="" width={100} alt="" /> */}
       </div>
 
       <ul className="flex w-3/4 justify-end gap-4 items-center border-b-[1px] border-gray-200">
@@ -67,12 +69,14 @@ const Header = () => {
           </Link>
         </li>
 
-        <li className="relative">
+        <li className="relative my-2">
           <span className="absolute z-10 text-sm font-bold bg-white text-black rounded-full w-6 flex justify-center font-3 -top-2 -right-2 items-center h-6 shadow">0</span>
-            <button className="p-3 rounded-full  bg-orange-400 font-bold"><ShoppingBag /></button>
+          <button onClick={()=> setOpenCart(true)} className="p-3 rounded-full  bg-orange-400 font-bold"><ShoppingBag /></button>
         </li>
       </ul>
     </header>
+    <Cart isOpen={openCart} onClose ={()=> setOpenCart(false)} />
+    </>
   );
 };
 
